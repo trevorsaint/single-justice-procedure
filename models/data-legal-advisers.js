10cb9ada5a38e69f23aeb72c9600f4585df40bef
Trevor Saint <trevor@infigomedia.uk>
2016-11-24 14:13:29 +0000

    Added more realistic data for 5 different cases for demo purposes

--- models/data-legal-advisers.js
+++ models/data-legal-advisers.js
@@ -31,7 +31,6 @@ var search = [
       "date": "2016-02-18",
       "offence-wording": "On 10 Nov 2016 at Belmont Hill, Lewisham, SE13, being a passenger on a Public Services Vehicle being operated on behalf of London Bus Company Limited , used for the carriage of passengers at separate fares, used a ticket which had been issued for use by another person on terms that it was not transferable. Contrary to regulation 7(1)(b) of the Public Service Vehicles (Conduct of Drivers, Inspectors, Conductors and Passengers) Regulations 1990 and section 25 of the Public Passenger Vehicles Act 1981",
       "statement-of-facts": "On Thursday 10th November 2016 you were on board a route 178 bus, garage code 552 with the intention of travelling from LEWISHAM to BLACKHEATH, a normal adult fare of &pound;2.40. During the course of this journey at about 4:15 PM you offered for the inspection of London Bus Inspector LB004512 a &pound;3.60 Discount SINGLE Pay as you go pass in oystercard format, serial number 02871229561, which had been issued to another person on terms that it was not transferable and was therefore not valid for travel by you. When questioned, you said &ldquo;It&rsquo;s my wife&rsquo;s&rdquo;. When informed that you were being reported you said &ldquo;It&rsquo;s not fair, nobody told me I couldn&rsquo;t use someone else&rsquo;s oystercard",
-      "witness-statement": "This statement is compiled from original notes made at 4:45 on 10 Nov 2016. On Thursday 10th November 2016 at 4:15 PM I was on duty in uniform at LEWISHAM HIGH ST, SE13 when I ticket checked a route 178 bus, garage code 552 on a journey in service to Woolwich. On boarding, the OYSTER CARD READERS WERE CHECKED. I approached a male passenger sitting on the upper deck, who I now know to be Mr David Francis of 6 Blessington Road, SE13, who was taking a journey from LEWISHAM to BLACKHEATH, a fare of £2.40. Mr Francis offered for inspection a £3.60 Discount SINGLE Pay as you go pass in oystercard format, serial number 02871229561. I observed that this pass was being shown without a valid photocard. I sealed this in evidence bag 46390 marked exhibit LO/1, which I now offer in evidence. I said to Mr Francis, &ldquo;is this the pass you are using for your journey?&rdquo;. He said &ldquo;Yes&rdquo;. I said, &ldquo;Do you have the photocard for this pass. These passes are not transferrable&rdquo;. He said &ldquo;It's my wife&rsquo;s&rdquo;. I asked him for his name and address, which he gave as Mr David Francis, 6 Blessington Road, Lewisham, London, SE13 5EB. These details were verified by voters check. I said &ldquo;I am withdrawing this OYSTER and I will be submitting a report to the Transport For London Prosecutions Team for their consideration. He said &ldquo;It&rsquo;s not fair, nobody told me I couldn&rsquo;t use someone else&rsquo;s oystercard&ldquo;",
       "mitigation": "",
       "plea": "No plea received",
       "decision": "Financial penalty",
@@ -50,23 +49,191 @@ var search = [
       "cost": "125",
       "surcharge": "30"
     },
-    "employment": {
-      "status":                    "Employed",
-      "national-insurance-number": "LC 59 17 17 B",
-      "employer-name":             "Ministry of Justice",
-      "employee-number":           "e902938480",
-      "employer-address": {
-        "address1": "102 Petty France",
-        "address2": "",
+    "related-documents": {
+      "document": [
+        {
+          "title":      "SJP notice",
+          "file-type":  "PDF",
+          "file-size":  "256KB",
+          "file-pages": "3 pages",
+          "file-path":  "/legal-adviser/documents/"
+        }
+      ]
+    }
+  },
+
+  {
+    "id": 2,
+    "personal-details": {
+      "title":      "Mr",
+      "firstname":  "Mohamed",
+      "lastname":   "Afadar",
+      "dob":        "1990-03-28",
+      "age":        "26",
+      "gender":     "Male",
+      "urn":        "90143862",
+      "address": {
+        "address1": "32 Llanover Road",
+        "address2": "Wembley",
         "town":     "London",
-        "postcode": "SW1H 9AJ"
+        "postcode": "HA9 7LJ"
       },
-      "employer-telephone":        "020 3334 3555",
-      "pay": {
-        "frequency": "",
-        "amount":    ""
+      "email":      "",
+      "phone":      "",
+      "mobile":     ""
+    },
+    "case-details": {
+      "status":        "No plea received",
+      "prosecutor":    "Transport for London",
+      "charge-date":   "2016-01-21",
+      "notice-served": "2016-11-14",
+    },
+    "offence": {
+      "title": "Rail &ndash; other by law offence",
+      "date": "2016-02-18",
+      "offence-wording": "On 5 Nov 2016 At Wembley Park  Where the entrance to or exit from a platform or station is via an automatic ticket barrier did enter or leave the station except with permission from an authorised person without passing through the barrier in the correct manner.",
+      "statement-of-facts": "On Saturday 5th November 2016 you were leaving Wembley Park underground station. You were observed to follow another passenger closely through the wide access ticket gate. When approached by LU Ticket Inspector LU043129, you offered for inspection a &pound;2.90 Zone 1-4 CHILD ticket. When questioned you said &ldquo;The machine must have given me the wrong ticket&rdquo;. When informed that you were being reported you said &ldquo;Whatever, I&rsquo;m not paying&rdquo;.",
+      "mitigation": "",
+      "plea": "No plea received",
+      "decision": "Financial penalty",
+      "collection-order-confirmed": "true",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "reason-for-attaching-to-earnings": "Compensation ordered",
+      "payment-method": "Pay direct to court",
+      "reserve-terms": "Lump sum within 14 days",
+      "defendant-pay": "Lump sum within 14 days",
+      "reason-for-not-deducting-from-benefits": "No information from the defendant",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "weekly-income-assumed": "440",
+      "fine-band-applied": "Band A",
+      "fine": "220",
+      "compensation": "5.90",
+      "cost": "125",
+      "surcharge": "30"
+    },
+    "related-documents": {
+      "document": [
+        {
+          "title":      "SJP notice",
+          "file-type":  "PDF",
+          "file-size":  "256KB",
+          "file-pages": "3 pages",
+          "file-path":  "/legal-adviser/documents/"
+        }
+      ]
+    }
+  },
+
+  {
+    "id": 3,
+    "personal-details": {
+      "title":      "Miss",
+      "firstname":  "Deborah",
+      "lastname":   "Jensen",
+      "dob":        "1995-07-02",
+      "age":        "21",
+      "gender":     "Female",
+      "urn":        "90117834",
+      "address": {
+        "address1": "31 Ruislip Road East",
+        "address2": "Greenford",
+        "town":     "London",
+        "postcode": "W13 0AZ"
       },
-      "receiving-benefits": "",
+      "email":      "",
+      "phone":      "",
+      "mobile":     ""
+    },
+    "case-details": {
+      "status":        "No plea received",
+      "prosecutor":    "Transport for London",
+      "charge-date":   "2016-01-21",
+      "notice-served": "2016-11-14",
+    },
+    "offence": {
+      "title": "Rail &ndash; other total evasion",
+      "date": "2016-11-07",
+      "offence-wording": "On 7 Nov 2016 At Ealing Broadway  Did enter a compulsory ticket area without having with her a valid ticket. Contrary to Byelaw 17(1) of the Transport for London Railway Byelaws Made under paragraph 26 of Schedule 11 to the Greater London Authority Act 1999 and confirmed under section 67 of the Transport Act 1962.",
+      "statement-of-facts": "On Monday 7th November 2016 you passed through the ticket gates at EALING BROADWAY intending to travel to EAST ACTON and were subsequently approached by a TfL ticket inspector LU012274. At 14:51 you produced a DISABLED PERSONS OYSTER FREEDOM PASS that did not belong to you. When questioned you admitted that this pass did belong to your partner.",
+      "mitigation": "",
+      "plea": "No plea received",
+      "decision": "Financial penalty",
+      "collection-order-confirmed": "true",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "reason-for-attaching-to-earnings": "Compensation ordered",
+      "payment-method": "Pay direct to court",
+      "reserve-terms": "Lump sum within 14 days",
+      "defendant-pay": "Lump sum within 14 days",
+      "reason-for-not-deducting-from-benefits": "No information from the defendant",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "weekly-income-assumed": "440",
+      "fine-band-applied": "Band A",
+      "fine": "220",
+      "compensation": "1.50",
+      "cost": "125",
+      "surcharge": "30"
+    },
+    "related-documents": {
+      "document": [
+        {
+          "title":      "SJP notice",
+          "file-type":  "PDF",
+          "file-size":  "256KB",
+          "file-pages": "3 pages",
+          "file-path":  "/legal-adviser/documents/"
+        }
+      ]
+    }
+  },
+
+  {
+    "id": 4,
+    "personal-details": {
+      "title":      "Miss",
+      "firstname":  "Marcel",
+      "lastname":   "Deveraux",
+      "dob":        "1996-02-11",
+      "age":        "20",
+      "gender":     "Male",
+      "urn":        "90278891",
+      "address": {
+        "address1": "36 Bushey Hill Road",
+        "address2": "Peckham",
+        "town":     "London",
+        "postcode": "SE5 8QJ"
+      },
+      "email":      "marceld@yahoo.fr",
+      "phone":      "",
+      "mobile":     "07844367821"
+    },
+    "case-details": {
+      "status":        "Awaiting decision",
+      "prosecutor":    "Transport for London",
+      "charge-date":   "2016-01-21",
+      "notice-served": "2016-11-16",
+    },
+    "offence": {
+      "title": "Public service vehicle &ndash; passenger used ticket issued for another person",
+      "date": "2016-11-07",
+      "offence-wording": "On 10 Nov 2016 at New Cross Road, New Cross, SE14, being a passenger on a Public Services Vehicle being operated on behalf of London Bus Company Limited , used for the carriage of passengers at separate fares, used a ticket which had been issued for use by another person on terms that it was not transferable. Contrary to regulation 7(1)(b) of the Public Service Vehicles (Conduct of Drivers, Inspectors, Conductors and Passengers) Regulations 1990 and section 25 of the Public Passenger Vehicles Act 1981.",
+      "statement-of-facts": "On Thursday 10th November 2016 you were on board a route 177 bus, garage code 308 with the intention of travelling from PECKHAM to NEW CROSS a normal adult fare of £2.40. During the course of this journey at about 7:48 AM you offered for the inspection of London Bus Inspector LB004192 a &pound;3.60 SINGLE Pay as you go pass in oystercard format, serial number 0287573061, which had been issued to another person on terms that it was not transferable and was therefore not valid for travel by you. When questioned, you said &ldquo;I borrowed my flatmate&rsquo;s card&rdquo;. When informed that you were being reported you said &ldquo;But it&rsquo;s paid for, why are you doing this, who does it help&rdquo;.",
+      "mitigation": "",
+      "plea": "Guilty",
+      "decision": "Financial penalty",
+      "collection-order-confirmed": "true",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "reason-for-attaching-to-earnings": "Compensation ordered",
+      "payment-method": "Pay direct to court",
+      "reserve-terms": "Lump sum within 14 days",
+      "defendant-pay": "Lump sum within 14 days",
+      "reason-for-not-deducting-from-benefits": "No information from the defendant",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "weekly-income-assumed": "600",
+      "fine-band-applied": "Band A",
+      "fine": "200",
+      "compensation": "2.40",
+      "cost": "125",
+      "surcharge": "30"
     },
     "related-documents": {
       "document": [
@@ -91,6 +258,89 @@ var search = [
           "file-pages": "3 pages",
           "file-path":  "/legal-adviser/documents/"
         }
+      ]
+    }
+  },
+
+  {
+    "id": 5,
+    "personal-details": {
+      "title":      "Mr",
+      "firstname":  "Jason",
+      "lastname":   "Harrington",
+      "dob":        "1994-05-16",
+      "age":        "22",
+      "gender":     "Male",
+      "urn":        "90387612",
+      "address": {
+        "address1": "5 Connaught Road",
+        "address2": "Woolwich",
+        "town":     "London",
+        "postcode": "SE18 6UQ"
+      },
+      "email":      "marceld@yahoo.fr",
+      "phone":      "",
+      "mobile":     "07465742198"
+    },
+    "case-details": {
+      "status":        "Awaiting decision",
+      "prosecutor":    "Transport for London",
+      "charge-date":   "2016-01-21",
+      "notice-served": "2016-11-14",
+    },
+    "offence": {
+      "title": "Public service vehicle &ndash; fare evasion",
+      "date": "2016-11-07",
+      "offence-wording": "On 6 Nov 2016 At Walworth Road SE17 Being a passenger on a Public service Vehicle operated on behalf of London Bus Services Limited being used for the carriage of passengers at separate fares where the vehicle was being operated by a Driver without a Conductor did not as directed by the Driver an Inspector or a Notice displayed on the vehicle pay the fare for the journey in accordance with the direction.",
+      "statement-of-facts": "On Sunday 6th November 2016 you were on board a route 68 bus, garage code 119, headed in the direction of WEST NORWOOD. During the course of this journey at about 1:17 PM you were approached by London Bus Inspector LB03897 and were not in possession of a valid ticket for travel. When questioned you said &ldquo;I can&rsquo;t afford to buy a ticket&rdquo;. When informed that you were being reported you said &ldquo;Do what you have to I guess&rdquo;.",
+      "mitigation": "",
+      "plea": "Guilty",
+      "decision": "Financial penalty",
+      "collection-order-confirmed": "true",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "reason-for-attaching-to-earnings": "Compensation ordered",
+      "payment-method": "Pay direct to court",
+      "reserve-terms": "Lump sum within 14 days",
+      "defendant-pay": "Lump sum within 14 days",
+      "reason-for-not-deducting-from-benefits": "No information from the defendant",
+      "reason-for-deducting-from-benefits": "Compensation ordered",
+      "weekly-income-assumed": "600",
+      "fine-band-applied": "Band A",
+      "fine": "40",
+      "compensation": "2.40",
+      "cost": "125",
+      "surcharge": "30"
+    },
+    "related-documents": {
+      "document": [
+        {
+          "title":      "SJP notice",
+          "file-type":  "PDF",
+          "file-size":  "256KB",
+          "file-pages": "3 pages",
+          "file-path":  "/legal-adviser/documents/"
+        },
+        {
+          "title":      "previous convictions",
+          "file-type":  "PDF",
+          "file-size":  "514KB",
+          "file-pages": "2 pages",
+          "file-path":  "/legal-adviser/documents/"
+        },
+        {
+          "title":      "statement of income and outgoings (MC100)",
+          "file-type":  "PDF",
+          "file-size":  "256KB",
+          "file-pages": "3 pages",
+          "file-path":  "/legal-adviser/documents/"
+        },
+        {
+          "title": "plea document",
+          "file-type": "PDF",
+          "file-size": "560KB",
+          "file-pages": "10 pages",
+          "file-path": "/legal-adviser/documents/",
+        },
       ]
     }
   }
